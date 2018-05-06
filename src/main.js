e476d6e35521ad8c889bc13e4fd25ca88e646c7c
Mariia Kononenko <mkon.masha@gmail.com>
2018-05-06 14:27:54 +0300

    ad

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import MainForm from './main-form/MainForm';
+
 
 import './styles/default.less';
 
 const init = () => {
   function render() {
     ReactDOM.render(
-      <MainForm currency={'₽'}
-                         lotPrice={109.7}
-                         lotAmount={25}/>,
+      <div>hello</div>,
       document.querySelector('#main-wrapper')
     )
   }
